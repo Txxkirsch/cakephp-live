888d7d9858fae26d4d092850ed6542601e89ba86
Steve <35690020+Txxkirsch@users.noreply.github.com>
2025-04-20 00:18:43 +0200

    Update WSComponent.php

--- src/Controller/Component/WSComponent.php
+++ src/Controller/Component/WSComponent.php
@@ -12,57 +12,58 @@ use ZMQContext;
  */
 class WSComponent extends Component
 {
-	/**
-	 * Default configuration.
-	 *
-	 * @var array
-	 */
-	protected array $_defaultConfig = [];
+    /**
+     * Default configuration.
+     *
+     * @var array
+     */
+    protected array $_defaultConfig = [];
 
-	/**
-	 *
-	 * @var int|null
-	 */
-	protected int|null $_currentConnectionId = null;
+    /**
+     *
+     * @var int|null
+     */
+    protected int|null $_currentConnectionId = null;
 
-	/**
-	 * @param array $config
-	 */
-	public function initialize(array $config): void
-	{
-		if (!empty($config['_Controller'])) {
-			$request                    = $config['_Controller']->getRequest();
-			$this->_currentConnectionId = $request->getHeader('X-WS-ID')[0] ?? null;
-			$this->_defaultTopic        = $config['_topic'];
-		}
-	}
+    protected string $_defaultTopic = 'default';
+    /**
+     * @param array $config
+     */
+    public function initialize(array $config): void
+    {
+        if (!empty($config['_Controller'])) {
+            $request                    = $config['_Controller']->getRequest();
+            $this->_currentConnectionId = $request->getHeader('X-WS-ID')[0] ?? null;
+            $this->_defaultTopic        = $config['_topic'];
+        }
+    }
 
-	/**
-	 * @param ComponentRegistry $registry
-	 * @param array $config
-	 */
-	public function __construct($registry = null, array $config = [])
-	{
-		parent::__construct($registry, $config);
-	}
+    /**
+     * @param ComponentRegistry $registry
+     * @param array $config
+     */
+    public function __construct($registry = null, array $config = [])
+    {
+        parent::__construct($registry, $config);
+    }
 
-	/**
-	 * @param $msg
-	 * @return mixed
-	 */
-	public function emit($msg, $topic = null)
-	{
-		$context = (new ZMQContext())->getSocket(ZMQ::SOCKET_PUSH);
-		$context->connect(Configure::read('Live.WS.internal'));
-		if (!empty($this->_currentConnectionId ?? 1)) {
-			$msg = [
-				'message'    => $msg,
-				'topic'      => $topic ?? $this->_defaultTopic,
-				'resourceId' => $this->_currentConnectionId ?? null,
-			];
-			return $context->send(json_encode($msg));
-		}
-		return false;
-	}
+    /**
+     * @param $msg
+     * @return mixed
+     */
+    public function emit($msg, $topic = null)
+    {
+        $context = (new ZMQContext())->getSocket(ZMQ::SOCKET_PUSH);
+        $context->connect(Configure::read('Live.WS.internal'));
+        if (!empty($this->_currentConnectionId ?? 1)) {
+            $msg = [
+                'message'    => $msg,
+                'topic'      => $topic ?? $this->_defaultTopic,
+                'resourceId' => $this->_currentConnectionId ?? null,
+            ];
+            return $context->send(json_encode($msg));
+        }
+        return false;
+    }
 
 }
